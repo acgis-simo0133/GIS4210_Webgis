e832a74e24fe590461a36cd12a533fae6b9f4e2c
acgis-simo0133 <simo0133@algonquinlive.com>
2018-04-15 21:49:08 -0400

    All current files April 15/18

---
+++ A1_JavaScript.js
@@ -0,0 +1,5 @@
+unction myFunction() {
+	document.getElementById("Control Form").innerHTML;
+	document.getElementById("Report_Status").innerHTML;
+	document.getElementById("Map_img").style.display = "block";
+}
